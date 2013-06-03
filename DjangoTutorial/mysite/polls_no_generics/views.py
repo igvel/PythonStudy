f7fa271aa409d613f3f8dbff89089870403e6158
Igor Elkin <igvel@mail.ru>
2013-06-03 19:07:05 +0400

    Generic views

--- DjangoTutorial/mysite/polls_no_generics/views.py
+++ DjangoTutorial/mysite/polls_no_generics/views.py
@@ -4,28 +4,34 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader, Context
-from django.views import generic
 from polls.models import Poll, Choice
 
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_poll_list'
+def index(request):
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    # Long way to load a template
+    # template = loader.get_template('polls/index.html')
+    # context = Context({
+    #             'latest_poll_list': latest_poll_list,
+    #             })
+    # return HttpResponse(template.render(context))
 
-    def get_queryset(self):
-        """Return the last five published polls."""
-        return Poll.objects.order_by('-pub_date')[:5]
+    # Shortcut for the code above
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
+def detail(request, poll_id):
+    # try:
+    #     poll = Poll.objects.get(pk=poll_id)
+    # except Poll.DoesNotExist:
+    #     raise Http404
+    # Shortcut
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
 
-class DetailView(generic.DetailView):
-    model = Poll
-    template_name = 'polls/detail.html'
-
-
-class ResultsView(generic.DetailView):
-    model = Poll
-    template_name = 'polls/results.html'
-
+def results(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/results.html', {'poll': poll})
 
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
